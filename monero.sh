c334b4347f7375a22f7e859e0b60750f5d1816fe
komenkx <44283571+komenkx@users.noreply.github.com>
2021-05-15 13:28:07 +0800

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-bionic-x64.tar.gz
-tar -xvf xmrig-6.8.1-bionic-x64.tar.gz
-./xmrig-6.8.1/xmrig -o pool.hashvault.pro:3333 -u 83MPNvPfe3zhCXasbE6jrpd9FRwH4MjrYdzbRtcYRxbx9T3m98Jg8akZAr4giKb8AUCk8JpLnAtHY9dnEHTMkfCGQnNiM4D.worke6 -k --tls -p x -t 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFB3D7dRLE4yfMsjU4aJ2sJrEe3S2GuMcx.test2 -p x --cpu 4
